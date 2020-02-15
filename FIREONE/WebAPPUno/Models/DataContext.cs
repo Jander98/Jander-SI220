9bee205f7f2e561189b5caf3d41363f6f9c8b368
Jander98 <jaromeror1998hotmail.com>
2020-02-15 12:02:57 -0400

    Primer MVC

---
+++ FIREONE/WebAPPUno/Models/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace WebAPPUno.Models
+{
+    public class DataContext :DbContext
+    {
+        public DataContext():base("DefaultConecction")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<WebAPPUno.Models.Person> People { get; set; }
+    }
+}
