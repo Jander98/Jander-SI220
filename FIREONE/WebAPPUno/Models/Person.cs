a493dd770a433191cbbd9cfa54683c0b6d3d2bd5
Jander98 <jaromeror1998hotmail.com>
2020-02-15 10:33:03 -0400

    Mi primer modelo

---
+++ FIREONE/WebAPPUno/Models/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebAPPUno.Models
+{
+    public class Person
+    {
+        [Key]
+        public int PersonID { get; set; }
+        [Required(ErrorMessage = "El nombre requerido")]
+        [Display(Name ="Nombre Completo")]
+        [StringLength(100,ErrorMessage ="Maximo 100 caracteres")]
+        public string Name { get; set; }
+        [Display(Name ="Carnet de Identidad")]
+        public string Document { get; set; }
+
+    }
+}
